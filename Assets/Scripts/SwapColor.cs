63b3e271b11f26bb80582125e52be3148bc4a6cd
KrzysiekGL <krzysztof.gl.szczerbowski@gmail.com>
2022-09-29 17:12:19 +0200

    Add animatos for Ptako

---
+++ Assets/Scripts/SwapColor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapColor : MonoBehaviour
+{
+		private Animator _animator;
+
+		void Start()
+		{
+				_animator = gameObject.GetComponent<Animator>();
+		}
+
+		void Update()
+		{
+				if (Input.GetKeyDown(KeyCode.DownArrow))
+				{
+						_animator.SetTrigger("SwapNext");
+				}
+		}
+}
