2f3664cd6e21f9857acca01209ed6718e962276b
KrzysiekGL <krzysztof.gl.szczerbowski@gmail.com>
2022-10-03 17:27:28 +0200

    Change spacing

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -2,42 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level : MonoBehaviour
-{
-		[SerializeReference]
-		private Obstacle obstacle;
+public class Level : MonoBehaviour {
+	[SerializeReference]
+	private Obstacle obstacle;
 
-		private float _timeElapsed = 0f;
-		private float _interval = 3f;
+	private float _timeElapsed = 0f;
+	private float _interval = 3f;
 
-		private Transform _levelTransform;
+	private Transform _levelTransform;
 
-		private bool _isGameRunning = false;
+	private bool _isGameRunning = false;
 
-		void Start()
-		{
-				_levelTransform = GetComponent<Transform>();
+	void Start() {
+		_levelTransform = GetComponent<Transform>();
+	}
+
+	void Update() {
+		// Press space to start the game
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			_isGameRunning = true;
 		}
 
-		void Update()
-		{
-			// Press space to start the game
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				_isGameRunning = true;
-			}
-
-			if (_isGameRunning)
-			{
-				// Generate obstacles after the interval
-				_timeElapsed += Time.deltaTime;
-				if (_timeElapsed >= _interval)
-				{
-					_timeElapsed = 0f;
-					_interval = Random.Range(3f, 4f);
-					Obstacle obstacleClone = Instantiate<Obstacle>(obstacle, _levelTransform.position, Quaternion.identity, _levelTransform.parent);
-					obstacleClone.transform.parent = _levelTransform;
-				}
+		if (_isGameRunning) {
+			// Generate obstacles after the interval
+			_timeElapsed += Time.deltaTime;
+			if (_timeElapsed >= _interval) {
+				_timeElapsed = 0f;
+				_interval = Random.Range(3f, 4f);
+				Obstacle obstacleClone = Instantiate<Obstacle>(obstacle, _levelTransform.position, Quaternion.identity, _levelTransform.parent);
+				obstacleClone.transform.parent = _levelTransform;
 			}
 		}
+	}
 }
